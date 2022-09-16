77bc7c040e7212bf648ae5f5e0b34df64a7f5480
Gustavo Bryan <devgustavob@gmail.com>
2022-09-15 22:20:08 -0300

    botoes funcionando

--- app/src/main/java/com/example/minorities_space/QuartaTela.kt
+++ app/src/main/java/com/example/minorities_space/QuartaTela.kt
@@ -1,11 +1,54 @@
 package com.example.minorities_space
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.example.minorities_space.databinding.ActivityQuartaTelaBinding
 
-class QuartaTela : AppCompatActivity() {
+class QuartaTela : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityQuartaTelaBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_quarta_tela)
+        binding = ActivityQuartaTelaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.icLgbt.setOnClickListener(this)
+        binding.icGender.setOnClickListener(this)
+        binding.icTrans.setOnClickListener(this)
+        binding.icHome.setOnClickListener(this)
+
     }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.ic_lgbt) {
+            novaTela()
+        } else if (view.id == R.id.ic_gender) {
+            novaTela1()
+        } else if (view.id == R.id.ic_trans) {
+            novaTela2()
+        } else if (view.id == R.id.ic_home) {
+            novaTela3()
+        }
+    }
+
+    private fun novaTela() {
+        startActivity(Intent(this, QuartaTela::class.java))
+    }
+
+    private fun novaTela1() {
+        startActivity(Intent(this, QuintaTela::class.java))
+    }
+
+    private fun novaTela2() {
+        startActivity(Intent(this, terceiratela::class.java))
+    }
+
+    private fun novaTela3() {
+        startActivity(Intent(this, SegundaTela::class.java))
+    }
+
 }
--- app/src/main/java/com/example/minorities_space/SegundaTela.kt
+++ app/src/main/java/com/example/minorities_space/SegundaTela.kt
@@ -11,28 +11,44 @@ class SegundaTela : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivitySegundaTelaBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         binding = ActivitySegundaTelaBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.icLgbt.setOnClickListener(this)
+        binding.icGender.setOnClickListener(this)
+        binding.icTrans.setOnClickListener(this)
+        binding.icHome.setOnClickListener(this)
+
     }
-        binding.home.setOnClickListener(this) {
-           val imgLgbt = Intent(this,MainActivity)
 
-    }        }
-        binding.imgLgbt.setOnClickListener(this)
-        binding.imgGender.setOnClickListener(this)
-        binding.imgTrans.setOnClickListener(this)
-
-        binding.buttonFrases.setOnClickListener(this) {
-            val Gerarfrases = (Intent(this, MainActivity::class.java))
-            startActivity(Gerarfrases)
+    override fun onClick(view: View) {
+        if (view.id == R.id.ic_lgbt) {
+            novaTela()
+        } else if (view.id == R.id.ic_gender) {
+            novaTela1()
+        } else if (view.id == R.id.ic_trans) {
+            novaTela2()
+        } else if (view.id == R.id.ic_home) {
+            novaTela3()
         }
+    }
 
+    private fun novaTela() {
+        startActivity(Intent(this, QuartaTela::class.java))
+    }
 
+    private fun novaTela1() {
+        startActivity(Intent(this, QuintaTela::class.java))
+    }
 
+    private fun novaTela2() {
+        startActivity(Intent(this, terceiratela::class.java))
+    }
 
+    private fun novaTela3() {
+        startActivity(Intent(this, SegundaTela::class.java))
+    }
 
-
-
-
+}
